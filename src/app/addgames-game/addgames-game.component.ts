a5dfccfb31d79999b89e432de3ef34b5a2a82c05
skkkyyyeee <anoopru2002@gmail.com>
2022-07-08 01:32:05 +0530

    addgameui

---
+++ src/app/addgames-game/addgames-game.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-addgames-game',
+  templateUrl: './addgames-game.component.html',
+  styleUrls: ['./addgames-game.component.css']
+})
+export class AddgamesGameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddgamesGameComponent } from './addgames-game/addgames-game.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddgamesGameComponent
   ],
   imports: [
     BrowserModule,
