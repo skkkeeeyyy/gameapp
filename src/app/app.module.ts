ff828c26e19f3fa720101a90e06f3acf9e548379
skkkyyyeee <anoopru2002@gmail.com>
2022-07-08 01:52:03 +0530

    finish

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddgamesGameComponent } from './addgames-game/addgames-game.component';
+import { ViewgameGameComponent } from './viewgame-game/viewgame-game.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddgamesGameComponent
+    AddgamesGameComponent,
+    ViewgameGameComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/viewgame-game/viewgame-game.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-viewgame-game',
+  templateUrl: './viewgame-game.component.html',
+  styleUrls: ['./viewgame-game.component.css']
+})
+export class ViewgameGameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
